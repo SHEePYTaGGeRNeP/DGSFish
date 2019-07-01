42c043c7598cecfc3c140a5e55aa04fd3c240533
Lorenzo <sheepytaggernep@gmail.com>
2019-07-01 07:47:28 +0200

    Added project setup + Die class.

---
+++ Fish Game/Assets/Scripts/Die.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assets.Scripts
+{
+    public class Die
+    {
+        private static readonly Random random = new Random();
+
+        public static readonly Die D4 = new Die(4);
+        public static readonly Die D6 = new Die(6);
+        public static readonly Die D8 = new Die(8);
+        public static readonly Die D10 = new Die(10);
+        public static readonly Die D12 = new Die(12);
+        public static readonly Die D20 = new Die(20);
+        public static readonly Die D100 = new Die(100);
+
+        public uint NrOfSides { get; }
+
+        public Die(uint nrOfSides)
+        {
+            this.NrOfSides = nrOfSides;
+        }
+
+
+        public static uint Roll(IEnumerable<KeyValuePair<Die, uint>> dice)
+        {
+            uint total = 0;
+            foreach (KeyValuePair< Die, uint> die in dice)
+            {
+                total += Roll(die.Value, die.Key.NrOfSides);
+            }
+            return total;
+        }
+        
+        public uint Roll() => Roll(1, this.NrOfSides);
+        public static uint Roll(uint nrOfSides) => Roll(1, nrOfSides);
+        public static uint Roll(uint nrOfDice, uint nrOfSides)
+        {
+            uint total = 0;
+            for (int i = 0; i < nrOfDice; i++)
+                total += (uint)random.Next(1, (int)nrOfSides + 1); ;
+            return total;
+        }
+
+        public override string ToString()
+        {
+            return $"d{this.NrOfSides}";
+        }
+
+    }
+}
---
+++ Fish Game/Assets/Unit Tests/DieTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts;
+using NUnit.Framework;
+
+namespace Assets.Unit_Tests
+{
+    public class DieTests
+    {
+        [Test]
+        public void Can_Create()
+        {
+            Die d = new Die(6);
+            Assert.NotNull(d);
+        }
+
+        [Test]
+        public void Can_Roll()
+        {
+            const uint sides = 6;
+            const uint nrOfDice = 1;
+            Die d = new Die(sides);
+            for (int i = 0; i < 100; i++)
+            {
+                uint result = d.Roll();
+                Assert.True(result > nrOfDice - 1 && result <= sides);
+            }
+        }
+
+        [Test]
+        public void Can_Roll_Static()
+        {
+            const uint sides = 6;
+            const uint nrOfDice = 1;
+            for (int i = 0; i < 100; i++)
+            {
+                uint result = Die.Roll(nrOfDice, sides);
+                Assert.True(result > nrOfDice - 1 && result <= sides);
+            }
+        }
+
+        [Test]
+        public void Can_Roll_Two_Static()
+        {
+            const uint sides = 6;
+            const uint nrOfDice = 2;
+            for (int i = 0; i < 100; i++)
+            {
+                uint result = Die.Roll(nrOfDice, sides);
+                Assert.True(result > nrOfDice - 1 && result <= sides * nrOfDice);
+            }
+        }
+
+        [Test]
+        public void Can_Roll_Dice()
+        {
+            KeyValuePair<Die, uint> die1 = new KeyValuePair<Die, uint>(new Die(8), 1);
+            KeyValuePair<Die, uint> die2 = new KeyValuePair<Die, uint>(new Die(8), 1);
+            KeyValuePair<Die, uint>[] dice = new KeyValuePair<Die, uint>[] { die1, die2 };
+            for (int i = 0; i < 100; i++)
+            {
+                uint result = Die.Roll(dice);
+                Assert.True(result > dice.Sum(x => x.Value) - 1
+                    && result <= dice.Sum(x => x.Value * x.Key.NrOfSides));
+            }
+        }
+    }
+}
