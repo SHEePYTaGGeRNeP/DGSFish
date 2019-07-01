d694a346188ff57ce82e248b852920170ebf7863
Lorenzo <lorenzo.suiker@dutchworkz.nl>
2019-07-01 11:18:20 +0200

    Random fish works.

--- Fish Game/Assets/Scripts/Game/FishHolder.cs
+++ Fish Game/Assets/Scripts/Game/FishHolder.cs
@@ -2,7 +2,7 @@
 
 namespace Assets.Scripts.Game
 {
-    [CreateAssetMenu(fileName = "Fish", menuName = "Fish/CreateFishHolder", order = 10)]
+    [CreateAssetMenu(fileName = "FishHolder", menuName = "Fish/CreateFishHolder", order = 10)]
     public class FishHolder : ScriptableObject
     {
         [SerializeField]
@@ -16,10 +16,35 @@ namespace Assets.Scripts.Game
         [SerializeField]
         private Fish[] _legendaryFish;
 
+        [SerializeField]
+        private int[] _chancePerType;
+
         public Fish RandomFish()
         {
             Fish[] type = this._commonFish;
+            float value = Random.Range(0, 101);
+            int extraAdd = 0;
+            for (int i = 0; i < _chancePerType.Length; i++)
+            {
+                if (value < _chancePerType[i] + extraAdd)
+                {
+                    type = IndexToType(i);
+                    break;
+                }
+                extraAdd += _chancePerType[i];
+            }
             return type[Random.Range(0, type.Length)];
+        }
+        private Fish[] IndexToType(int index)
+        {
+            switch (index)
+            {
+                default: return this._commonFish;
+                case 1: return this._uncommonFish;
+                case 2: return this._rareFish;
+                case 3: return this._veryRareFish;
+                case 4: return this._legendaryFish;
+            }
         }
     }
 }
---
+++ Fish Game/Assets/Scripts/TestRandomFish.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Scripts.Game;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    class TestRandomFish : MonoBehaviour
+    {
+        [SerializeField]
+        private FishHolder _fishHolder;
+
+        public void Test()
+        {
+            this._fishHolder.RandomFish();
+        }
+    }
+}
