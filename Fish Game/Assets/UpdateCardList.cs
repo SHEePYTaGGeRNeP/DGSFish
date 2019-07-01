a63587297a88678ea45348d1510968099c9d9e91
valourus <koen.vdels@xs4all.nl>
2019-07-01 14:17:10 +0200

    started work on the battle scene

---
+++ Fish Game/Assets/UpdateCardList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.Game;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateCardList : MonoBehaviour
+{
+    private Dropdown _dropdown;
+    
+    private void Awake()
+    {
+        _dropdown = GetComponent<Dropdown>();
+    }
+
+    public void updateList()
+    {
+        GameSystem.Instance.CurrentPlayer.Cards.Where(card => card.CanPlay()).ToList().ForEach(card =>
+        {
+//            card.
+        });
+    }
+}
