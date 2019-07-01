7ea5da8e54b1a30e4f51108bf0c69a93125c8eac
Lorenzo <lorenzo.suiker@dutchworkz.nl>
2019-07-01 16:50:15 +0200

    Added searching for cards

--- Fish Game/Assets/Scripts/Game/Cards/CardMainMenuButton.cs
+++ Fish Game/Assets/Scripts/Game/Cards/CardMainMenuButton.cs
@@ -1,19 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets.Scripts.Game.Cards
 {
-    class CardMainMenuButton
+    class CardMainMenuButton : MonoBehaviour
     {
         [SerializeField]
         private CardHolder _holder;
+
+        [SerializeField]
+        private float _percChanceToFindCard = 0.33f;
+
         public void On_Click()
         {
-            GameSystem.Instance.CurrentPlayer.AddCard(this._holder.RandomCard());
+            if (Random.Range(0, 1) > _percChanceToFindCard)
+            {
+                ConsoleLog.AddToLog($"{GameSystem.Instance.CurrentPlayer.Name} found no card.");
+                GameSystem.Instance.NextPlayer();
+                return;
+            }
+            Card c = this._holder.RandomCard();
+            GameSystem.Instance.CurrentPlayer.AddCard(c);
+            ConsoleLog.AddToLog($"{GameSystem.Instance.CurrentPlayer.Name} found {c.Name}.");
             GameSystem.Instance.NextPlayer();
         }
     }
