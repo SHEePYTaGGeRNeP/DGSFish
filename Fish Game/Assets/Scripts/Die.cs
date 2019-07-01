d3ff570fe9913d6f836b17b6bdf783ac0204ca7e
Lorenzo <lorenzo.suiker@dutchworkz.nl>
2019-07-01 10:22:31 +0200

    Added Fish ScriptableObject

--- Fish Game/Assets/Scripts/Die.cs
+++ Fish Game/Assets/Scripts/Die.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
+    [Serializable]
     public class Die
     {
-        private static readonly Random random = new Random();
-
         public static readonly Die D4 = new Die(4);
         public static readonly Die D6 = new Die(6);
         public static readonly Die D8 = new Die(8);
@@ -15,7 +15,13 @@ namespace Assets.Scripts
         public static readonly Die D20 = new Die(20);
         public static readonly Die D100 = new Die(100);
 
-        public uint NrOfSides { get; }
+        [SerializeField]
+        private uint _nrOfSides;
+        public uint NrOfSides
+        {
+            get => this._nrOfSides;
+            set => this._nrOfSides = value;
+        }
 
         public Die(uint nrOfSides)
         {
@@ -26,20 +32,20 @@ namespace Assets.Scripts
         public static uint Roll(IEnumerable<KeyValuePair<Die, uint>> dice)
         {
             uint total = 0;
-            foreach (KeyValuePair< Die, uint> die in dice)
+            foreach (KeyValuePair<Die, uint> die in dice)
             {
                 total += Roll(die.Value, die.Key.NrOfSides);
             }
             return total;
         }
-        
+
         public uint Roll() => Roll(1, this.NrOfSides);
         public static uint Roll(uint nrOfSides) => Roll(1, nrOfSides);
         public static uint Roll(uint nrOfDice, uint nrOfSides)
         {
             uint total = 0;
             for (int i = 0; i < nrOfDice; i++)
-                total += (uint)random.Next(1, (int)nrOfSides + 1); ;
+                total += (uint)UnityEngine.Random.Range(1, (int)nrOfSides + 1); ;
             return total;
         }
 
---
+++ Fish Game/Assets/Scripts/Game/Fish.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    [CreateAssetMenu(fileName = "Fish", menuName = "Fish/CreateFish", order = 0)]
+    public class Fish : ScriptableObject
+    {
+        [SerializeField]
+        private string _name;
+        public string Name => this._name;
+
+        [SerializeField]
+        private Sprite _image;
+        public Sprite Image => this._image;
+
+        public bool IsKO { get; set; }
+
+        [SerializeField]
+        private int _baseDamage;
+        public int BaseDamage => this._baseDamage;
+
+        [SerializeField]
+        private Die _damageDie;
+        public Die DamageDie => this._damageDie;
+
+        [SerializeField]
+        private uint _amountOfDamageDie;
+        public uint AmountOfDamageDie => this._amountOfDamageDie;
+
+        [SerializeField]
+        private Die _catchDie;
+        public Die CatchDie => this._catchDie;
+
+        [SerializeField]
+        private int[] _catchValues;
+        public int[] CatchValues => this._catchValues;
+    }
+}
